e5914f8ba7e77c9c85d60ed63b4b4cf4691ca65e
SHAIR YAAR KHAN <shairyaark932@gmail.com>
2026-06-06 15:43:03 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.routes.js'
@@ -7,6 +8,10 @@ import messageRoutes from './routes/message.routes.js'
 dotenv.config()
 
 const app = express()
+
+const __dirname = path.resolve();
+console.log(__dirname)
+
 const PORT = process.env.PORT || 3000
 
 
@@ -14,8 +19,14 @@ app.use('/api/auth',authRoutes)
 app.use('/api/message',messageRoutes)
 
 
+//make ready for deployment
+if(process.env.NODE_ENV === "production"){
+    console.log(process.env.NODE_ENV)
+    app.use(express.static(path.join(__dirname,"../frontend/vite-project/dist")))
 
-
-
+    app.get(/.*/,(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/vite-project/dist/index.html"))
+    })
+}
 
 app.listen(PORT,()=> {console.log("Serever running on PORT "+PORT)})
