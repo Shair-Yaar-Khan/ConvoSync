a71620894718205411114c0347286ab6f017d918
SHAIR YAAR KHAN <shairyaark932@gmail.com>
2026-06-08 08:35:56 +0530

    update profile endpoint added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js"
 import bcrypt from 'bcryptjs'
 import {generateToken} from '../lib/utils.js'
+import cloudinary from "../lib/cloudinary.js"
+import { ENV } from "../lib/env.js"
 
 export const signup =async (req,res)=>{
     const {fullName,email,password} = req.body
@@ -94,4 +96,25 @@ export const login = async (req,res)=>{
 export const logout = async (req,res)=>{
        res.cookie("token","",{maxAge:0})
        res.status(200).json({message:"User Loggedout successfully!"})
+}
+
+export const updateProfile = async (req,res) =>{
+    
+    try {
+        const {profilePic} = req.body
+
+        if(!profilePic) return res.status(400).json({message:"Profile pic is required"})
+
+         const userId = req.user._id
+         
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
+
+        res.status(200).json({updatedUser})
+
+    } catch (error) {
+        console.log("Error in update profile:", error)
+        res.status(500).json({message:"Internal server error"})
+    }
 }
